64891607b315baa627dd8bdc6a079a8061fde375
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-06-06 16:11:39 +0300

    some modification on genersting random value

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
 import Dice from "./components/Dice";
 import "./index.css";
+
 export default function App() {
-  const [diceArray, setDiceArray] = useState(randDiceGenerator);
   function randDiceGenerator() {
+    let newArray = [];
     for (let i = 0; i < 10; i++) {
-      diceArray.push(Math.ceil(Math.random() * 6));
+      newArray.push(Math.ceil(Math.random() * 6));
     }
+    return newArray;
+  }
+  const [diceArray, setDiceArray] = useState(randDiceGenerator());
+  function setNewValue() {
+    setDiceArray(randDiceGenerator());
   }
 
   const dice = diceArray.map((die, i) => {
     return <Dice key={i} value={die} />;
   });
+
   return (
     <div className='App'>
       <div className='dice-container'>{dice}</div>
-      <button
-        onClick={() =>
-          setDiceArray(diceArray.push(Math.ceil(Math.random() * 6)))
-        }
-        className='roll-btn'
-      >
+      <button onClick={setNewValue} className='roll-btn'>
         Roll
       </button>
     </div>
